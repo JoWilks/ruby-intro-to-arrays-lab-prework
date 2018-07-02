f9e98948991014aee487819223fca7b87aa0ede4
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 14:38:44 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,17 @@
 def instantiate_new_array
   Array.new
 end
+
+def array_with_two_elements
+  array.new("a", "b")
+end
+
+def first_element
+  array[0]
+end
+
+def third_element
+  array[2]
+end
+
+def 
